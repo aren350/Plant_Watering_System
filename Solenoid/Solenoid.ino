31735f86f40380dfb214f31b325df9517550336b
aren350 <arenrohan@gmail.com>
2018-11-22 20:44:19 -0800

    Add files via upload

---
+++ Solenoid/Solenoid.ino
@@ -0,0 +1,13 @@
+int solenoidPin=4;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(solenoidPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(solenoidPin, HIGH);
+  delay(15000);
+  digitalWrite(solenoidPin,LOW);
+  delay(30000);
+}
